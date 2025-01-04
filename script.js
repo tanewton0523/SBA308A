6b1feca42fc23ca5e33cc84b73a76c678ca6df19
tanewton0523 <59624833+tanewton0523@users.noreply.github.com>
2025-01-04 18:49:53 -0500

    Create SBA308A

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("searchCharBtn").addEventListener("click", getCharData);
+document.getElementById("searchSpellBtn").addEventListener("click", getSpellData);
+document.getElementById("searchBookBtn").addEventListener("click", getBookData);
+
